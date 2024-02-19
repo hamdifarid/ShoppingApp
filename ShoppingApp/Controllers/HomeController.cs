9e0177290ad0bc63a9906bc3326e11260048760c
hamdifarid <46102589+hamdifarid@users.noreply.github.com>
2024-02-19 20:40:05 +0530

    Update HomeController.cs

--- ShoppingApp/Controllers/HomeController.cs
+++ ShoppingApp/Controllers/HomeController.cs
@@ -22,7 +22,6 @@ namespace ShoppingApp.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
